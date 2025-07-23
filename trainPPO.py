e9bbc2f9ef07b1a7e5dff2f5087adc28a65a17de
QJ72 <quentin.jouanjan@gmail.com>
2025-07-23 15:24:51 +0200

    add training, untested

---
+++ trainPPO.py
@@ -0,0 +1,178 @@
+import argparse
+
+from isaaclab.app import AppLauncher
+
+# add argparse arguments
+parser = argparse.ArgumentParser(description="Random agent for Isaac Lab environments.")
+# append AppLauncher cli args
+AppLauncher.add_app_launcher_args(parser)
+# parse the arguments
+args_cli = parser.parse_args()
+
+# launch omniverse app
+app_launcher = AppLauncher(args_cli)
+simulation_app = app_launcher.app
+
+from typing import Union, Optional
+
+from tqdm import trange
+
+import torch
+import torch.optim as optim
+
+import gymnasium
+import numpy as np
+
+from RLAlg.alg.ppo import PPO
+from RLAlg.buffer.replay_buffer import ReplayBuffer, compute_gae
+from RLAlg.nn.steps import StochasticContinuousPolicyStep, DiscretePolicyStep, ValueStep
+from model.modelPPO import Actor, Critic
+
+from env.nao_grab_env_cfg import NaoGrabEnvCfg
+
+def process_obs(obs):
+    features = obs["policy"]
+
+    return features
+
+class Trainer:
+    def __init__(self):
+
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        
+        self.envs = gymnasium.make("NaoGrabEnv-v0", cfg=NaoGrabEnvCfg())
+
+        self.env_nums, self.obs_dim = self.envs.observation_space.shape
+
+        self.steps = 50
+        self.buffer_steps = 1000
+        self.rollout_steps = self.steps
+
+        max_action = torch.from_numpy(self.envs.single_action_space.high).float()
+        obs_space = self.envs.single_observation_space.shape
+        action_space = self.envs.single_action_space.shape
+
+        self.actor = Actor(np.prod(obs_space), np.prod(action_space), [128, 128], max_action)
+        self.critic = Critic(np.prod(obs_space), [128, 128])
+        
+        self.optimizer = optim.Adam(
+            list(self.actor.parameters()) + list(self.critic.parameters()), lr=3e-4
+        )
+        
+        self.replay_buffer = ReplayBuffer(self.env_nums, self.steps, device=self.device)
+        self.replay_buffer.create_storage_space("observations", obs_space, torch.float32)
+        self.replay_buffer.create_storage_space("actions", action_space, torch.float32)
+        self.replay_buffer.create_storage_space("log_probs", (), torch.float32)
+        self.replay_buffer.create_storage_space("rewards", (), torch.float32)
+        self.replay_buffer.create_storage_space("values", (), torch.float32)
+        self.replay_buffer.create_storage_space("dones", (), torch.float32)
+        
+        self.batch_keys = ["observations", "actions", "log_probs", "rewards", "values", "returns", "advantages"]
+        
+        self.gamma = 0.99
+        self.lambda_ = 0.95
+        self.clip_ratio = 0.2
+        self.regularization_weight = 0.0
+        self.max_grad_norm = 1.0
+        self.value_loss_weight = 0.5
+        self.entropy_weight = 0.01
+    
+    @torch.no_grad()
+    def get_action(self, obs:np.ndarray):
+        obs = torch.from_numpy(obs).float().to(self.device)
+        actor_step:Union[StochasticContinuousPolicyStep, DiscretePolicyStep]  = self.actor(obs)
+        value_step:ValueStep = self.critic(obs)
+        
+        action = actor_step.action
+        log_prob = actor_step.log_prob
+        value = value_step.value
+        
+        return action, log_prob, value
+    
+    def average_non_zero(self, numbers):
+        non_zero_numbers = [num for num in numbers if num != 0]
+        if not non_zero_numbers:
+            return 0  # Return 0 if there are no non-zero elements
+        return sum(non_zero_numbers) / len(non_zero_numbers)
+    
+    def rollout(self):
+        obs = self.obs
+        for i in range(self.rollout_steps):
+            action, log_prob, value = self.get_action(obs)
+            next_obs, reward, done, timeout, info = self.envs.step(action)
+            
+            record = {
+                "observations": obs,
+                "actions": action,
+                "log_probs": log_prob,
+                "rewards": reward,
+                "values": value,
+                "dones": done
+            }
+            
+            self.replay_buffer.add_records(record)
+            
+            obs = next_obs
+            
+            if "episode" in info:
+                print(self.average_non_zero(info['episode']['r']))
+        
+        self.obs = obs
+        _, _, value = self.get_action(obs)
+        returns, advantages = compute_gae(
+            self.replay_buffer.data["rewards"],
+            self.replay_buffer.data["values"],
+            self.replay_buffer.data["dones"],
+            value,
+            self.gamma,
+            self.lambda_
+            )
+        
+        self.replay_buffer.add_storage("returns", returns)
+        self.replay_buffer.add_storage("advantages", advantages)
+        
+    def update(self, num_iteration:int, batch_size:int):
+        for _ in range(num_iteration):
+            for batch in self.replay_buffer.sample_batchs(self.batch_keys, batch_size):
+                obs_batch = batch["observations"].to(self.device)
+                action_batch = batch["actions"].to(self.device)
+                log_prob_batch = batch["log_probs"].to(self.device)
+                value_batch = batch["values"].to(self.device)
+                return_batch = batch["returns"].to(self.device)
+                advantage_batch = batch["advantages"].to(self.device)
+
+                policy_loss, entropy, kl_divergence = PPO.compute_policy_loss(self.actor, log_prob_batch, obs_batch, action_batch, advantage_batch, self.clip_ratio, self.regularization_weight)
+ 
+
+                value_loss = PPO.compute_clipped_value_loss(self.critic, obs_batch, value_batch, return_batch, self.clip_ratio)
+                
+                loss = policy_loss + value_loss * self.value_loss_weight - entropy * self.entropy_weight
+
+                self.optimizer.zero_grad()
+                loss.backward()
+                torch.nn.utils.clip_grad_norm_(self.actor.parameters(), self.max_grad_norm)
+                torch.nn.utils.clip_grad_norm_(self.critic.parameters(), self.max_grad_norm)
+                self.optimizer.step()
+    
+    def save_models(self):
+        torch.save(self.actor.state_dict(), 'PPOactor.pth')
+        torch.save(self.critic.state_dict(), 'PPOcritic.pth')
+                
+    def train(self, num_epoch:int, num_iteration:int, batch_size:int):
+        self.obs, _ = self.envs.reset()
+        
+        for _ in trange(num_epoch):
+            self.rollout()
+            self.update(num_iteration, batch_size)
+        
+
+def main():
+    trainer = Trainer()
+
+    trainer.train()
+
+    trainer.envs.close()
+
+if __name__ == "__main__":
+    main()
+    simulation_app.close()
---
+++ trainSAC.py
@@ -0,0 +1,175 @@
+import argparse
+
+from isaaclab.app import AppLauncher
+
+# add argparse arguments
+parser = argparse.ArgumentParser(description="Random agent for Isaac Lab environments.")
+# append AppLauncher cli args
+AppLauncher.add_app_launcher_args(parser)
+# parse the arguments
+args_cli = parser.parse_args()
+
+# launch omniverse app
+app_launcher = AppLauncher(args_cli)
+simulation_app = app_launcher.app
+
+from typing import Union, Optional
+
+from tqdm import trange
+
+import torch
+import torch.optim as optim
+
+import gymnasium
+import numpy as np
+
+from RLAlg.alg.sac import SAC
+from RLAlg.buffer.replay_buffer import ReplayBuffer
+from RLAlg.nn.steps import StochasticContinuousPolicyStep, DiscretePolicyStep, ValueStep
+from model.modelPPO import Actor, Critic
+
+from env.nao_grab_env_cfg import NaoGrabEnvCfg
+
+def process_obs(obs):
+    features = obs["policy"]
+
+    return features
+
+class Trainer:
+    def __init__(self):
+
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        
+        self.envs = gymnasium.make("NaoGrabEnv-v0", cfg=NaoGrabEnvCfg())
+
+        self.env_nums, self.obs_dim = self.envs.observation_space.shape
+
+        self.steps = 50
+        self.buffer_steps = 1000
+        self.rollout_steps = self.steps
+
+        self.max_action = torch.from_numpy(self.envs.single_action_space.high).float()
+        obs_space = self.envs.single_observation_space.shape
+        action_space = self.envs.single_action_space.shape
+
+        self.actor = Actor(np.prod(obs_space), np.prod(action_space), [128, 128], self.max_action)
+        self.critic = Critic(np.prod(obs_space), np.prod(action_space), [128, 128])
+        self.critic_target = Critic(np.prod(obs_space), np.prod(action_space), [128, 128])
+        self.critic_target.load_state_dict(self.critic.state_dict())
+
+        for param in self.critic_target.parameters():
+            param.requires_grad = False
+
+        self.actor_optimizer = optim.Adam(self.actor.parameters(), lr=3e-4)
+        self.critic_optimizer = optim.Adam(self.critic.parameters(), lr=3e-4)
+        
+        self.replay_buffer = ReplayBuffer(self.env_nums, self.buffer_steps, device=self.device)
+        self.replay_buffer.create_storage_space("observations", obs_space, torch.float32)
+        self.replay_buffer.create_storage_space("next_observations", obs_space, torch.float32)
+        self.replay_buffer.create_storage_space("actions", action_space, torch.float32)
+        self.replay_buffer.create_storage_space("rewards", (), torch.float32)
+        self.replay_buffer.create_storage_space("dones", (), torch.float32)
+
+        self.batch_keys = ["observations", "next_observations", "actions", "rewards", "dones"]
+        
+        self.gamma = 0.99
+        self.alpha = 0.2
+        self.regularization_weight = 0.0
+        self.tau = 0.005
+        self.max_grad_norm = 1.0
+    
+    @torch.no_grad()
+    def get_action(self, obs:np.ndarray, random:bool=False):
+        obs = torch.from_numpy(obs).float().to(self.device)
+        actor_step:Union[StochasticContinuousPolicyStep, DiscretePolicyStep]  = self.actor(obs)
+        
+        if random:
+            action = actor_step.action.uniform_(-1, 1) * self.max_action
+        else:
+            action = actor_step.action
+            
+        return action.tolist()
+    
+    def average_non_zero(self, numbers):
+        non_zero_numbers = [num for num in numbers if num != 0]
+        if not non_zero_numbers:
+            return 0  # Return 0 if there are no non-zero elements
+        return sum(non_zero_numbers) / len(non_zero_numbers)
+    
+    def rollout(self, random:bool=False):
+        obs = self.obs
+        for i in range(self.rollout_steps):
+            action = self.get_action(obs, random)
+            next_obs, reward, done, timeout, info = self.envs.step(action)
+            
+            record = {
+                "observations": obs,
+                "next_observations": next_obs,
+                "actions": action,
+                "rewards": reward,
+                "dones": done
+            }
+            
+            self.replay_buffer.add_records(record)
+            
+            obs = next_obs
+            
+            if "episode" in info:
+                print(self.average_non_zero(info['episode']['r']))
+        
+        self.obs = obs
+        
+    def update(self, num_iteration:int, batch_size:int):
+        for _ in range(num_iteration):
+            batch = self.replay_buffer.sample_batch(self.batch_keys, batch_size)
+            obs_batch = batch["observations"].to(self.device)
+            next_obs_batch = batch["next_observations"].to(self.device)
+            action_batch = batch["actions"].to(self.device)
+            reward_batch = batch["rewards"].to(self.device)
+            done_batch = batch["dones"].to(self.device)
+            
+            self.critic_optimizer.zero_grad(set_to_none=True)
+            critic_loss = SAC.compute_critic_loss(self.actor, self.critic, self.critic_target, obs_batch, action_batch, reward_batch,
+                                                  next_obs_batch, done_batch, self.alpha, self.gamma)
+            critic_loss.backward()
+            torch.nn.utils.clip_grad_norm_(self.critic.parameters(), self.max_grad_norm)
+            self.critic_optimizer.step()
+
+            for param in self.critic.parameters():
+                param.requires_grad = False
+
+            self.actor_optimizer.zero_grad(set_to_none=True)
+            actor_loss = SAC.compute_actor_loss(self.actor, self.critic, obs_batch.detach(), self.alpha, self.regularization_weight)
+            actor_loss.backward()
+            torch.nn.utils.clip_grad_norm_(self.actor.parameters(), self.max_grad_norm)
+            self.actor_optimizer.step()
+
+            for param in self.critic.parameters():
+                param.requires_grad = True
+
+            SAC.update_target_param(self.critic, self.critic_target, self.tau)
+                
+    def train(self, num_epoch:int, num_iteration:int, batch_size:int):
+        self.obs, _ = self.envs.reset()
+        random = True
+        for i in trange(num_epoch):
+            if i > (num_epoch // 10):
+                random = False
+            self.rollout(random)
+            self.update(num_iteration, batch_size)
+    
+    def save_models(self):
+        torch.save(self.actor.state_dict(), 'SACactor.pth')
+        torch.save(self.critic.state_dict(), 'SACcritic.pth')
+        
+
+def main():
+    trainer = Trainer()
+
+    trainer.train()
+
+    trainer.envs.close()
+
+if __name__ == "__main__":
+    main()
+    simulation_app.close()
